7ad4845b55b2977127cecfba85f76f772846e35d
Jon Adams <jon@kuokoa.studio>
2020-05-27 11:49:40 -1000

    export address; bump version

--- build/main/index.js
+++ build/main/index.js
@@ -3,10 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Message = exports.Header = exports.Envelope = exports.Mailbox = void 0;
+exports.Message = exports.Header = exports.Envelope = exports.Mailbox = exports.Address = void 0;
 const client_1 = __importDefault(require("./client"));
 const mailbox_1 = require("./mailbox");
 Object.defineProperty(exports, "Mailbox", { enumerable: true, get: function () { return mailbox_1.SelectedMailbox; } });
+const address_1 = __importDefault(require("./address"));
+exports.Address = address_1.default;
 const envelope_1 = __importDefault(require("./envelope"));
 exports.Envelope = envelope_1.default;
 const message_1 = __importDefault(require("./message"));
--- build/module/index.js
+++ build/module/index.js
@@ -1,7 +1,8 @@
 import Client from './client';
 import { SelectedMailbox as Mailbox } from './mailbox';
+import Address from './address';
 import Envelope from './envelope';
 import Message from './message';
 import Header from './header';
-export { Mailbox, Envelope, Header, Message };
+export { Address, Mailbox, Envelope, Header, Message };
 export default Client;
